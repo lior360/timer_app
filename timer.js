220e8d31512ee24da8a37e6947ff0e6409085fc1
Lior Shalev <liorshalev6@gmail.com>
2022-04-26 15:48:23 +0300

    Binding Events in a Class

--- timer.js
+++ timer.js
@@ -1,12 +1,31 @@
-const timer = document.querySelector('#duration');
-let duration = parseInt(timer.value);
+// const timer = document.querySelector('#duration');
+// let duration = parseInt(timer.value);
 
-timer.addEventListener('change',()=>{
-    if(parseInt(timer.value)>0){
-        duration = parseInt(timer.value);
+// timer.addEventListener('change',()=>{
+//     if(parseInt(timer.value)>0){
+//         duration = parseInt(timer.value);
+//     }
+//     else{
+//         alert('timer shuld be a number larger then 0');
+//         timer.value = duration;
+//     }
+// })
+class Timer{
+    constructor(durationInput,startButton,pauseButton){
+        this.durationInput = durationInput;
+        this.startButton  = startButton;
+        this.pauseButton= pauseButton;
+
+        this.startButton.addEventListener('ckick',this.start);
+    } 
+
+    start(){
+        console.log('test');
     }
-    else{
-        alert('timer shuld be a number larger then 0');
-        timer.value = duration;
-    }
-})
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput,startButton,pauseButton);
