99d6981bb3cce601077acd4c50736dd91942a2f3
Lior Shalev <liorshalev6@gmail.com>
2022-04-26 19:51:15 +0300

    Starting and Pausing the Timer

--- timer.js
+++ timer.js
@@ -1,31 +1,31 @@
-// const timer = document.querySelector('#duration');
-// let duration = parseInt(timer.value);
-
-// timer.addEventListener('change',()=>{
-//     if(parseInt(timer.value)>0){
-//         duration = parseInt(timer.value);
-//     }
-//     else{
-//         alert('timer shuld be a number larger then 0');
-//         timer.value = duration;
-//     }
-// })
 class Timer{
-    constructor(durationInput,startButton,pauseButton){
+    constructor(durationInput, startButton, pauseButton) {
         this.durationInput = durationInput;
-        this.startButton  = startButton;
-        this.pauseButton= pauseButton;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('ckick',this.start);
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     } 
 
-    start(){
-        console.log('test');
+    start = () => {
+        this.tick();
+        this.interval =setInterval(this.tick,1000);
     }
-}
 
+    pause = ()=>{
+        clearInterval(this.interval);
+    }
+
+    tick = ()=>{
+        console.log('tick')
+    }
+
+    
+}
+ //get all inputs from the body
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
-
+//create new timer obj
 const timer = new Timer(durationInput,startButton,pauseButton);
