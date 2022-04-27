d1df256b913f1b025a1a7bc481f7e004899b399c
Lior Shalev <liorshalev6@gmail.com>
2022-04-27 12:29:52 +0300

    finishing animation, final app!

--- timer.js
+++ timer.js
@@ -1,16 +1,24 @@
 class Timer{
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton,callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if(callbacks){
+            this.onStart= callbacks.onStart;
+            this.onTick=callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     } 
 
     start = () => {
+        if(this.onStart){
+            this.onStart(this.timeRemaining);
+        }
         this.tick();
-        this.interval =setInterval(this.tick,1000);
+        this.interval =setInterval(this.tick,50);
     }
 
     pause = ()=>{
@@ -24,10 +32,16 @@ class Timer{
         this.startButton.disabled = true;
         const timeRemaining = this.timeRemaining;
         if (timeRemaining>0){
-            this.timeRemaining = timeRemaining-1;
+            this.timeRemaining = timeRemaining-0.05;
+            if (this.onTick){
+                this.onTick(this.timeRemaining);
+            }
         }
         else{
             this.pause();
+            if(this.onComplete){
+                this.onComplete();
+            }
         }
     }//tick
 
@@ -36,13 +50,8 @@ class Timer{
     }
 
     set timeRemaining(time){
-        this.durationInput.value=time;
+        this.durationInput.value=time.toFixed(2);
     }
     
 }
- //get all inputs from the body
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
-//create new timer obj
-const timer = new Timer(durationInput,startButton,pauseButton);
+ 
